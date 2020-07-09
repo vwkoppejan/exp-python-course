1db687827c1f326e4fc7d0d52a3c3dc3383e5421
Jelle <J.F.deJong@hotmail.com>
2020-07-09 15:43:32 +0200

    columnSelection functie toegevoegd aan preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -3,3 +3,21 @@ import pandas as pd
 def importFile(file):
     df = pd.read_csv(file, encoding = "ISO-8859-1")
     return df
+
+
+def columnSelection(df=None, columnsToKeep=None):
+    # Selects the relevant columns for this project
+    # df = dataframe
+    # columnsToKeep = list containing column names
+
+    # Pre-selection
+    sizeBefore = df.shape[0]
+    df = df[df['seller']=='privat'] 
+    df = df[df['offerType']=='Angebot']
+    sizeAfter = df.shape[0]
+    print('Size before pre-selection: ' + str(sizeBefore) + '. Size after pre-selection: ' + str(sizeAfter) + '.')
+
+    # Only keep selected columns
+    df = df.loc[:, columnsToKeep]  
+
+    return df
