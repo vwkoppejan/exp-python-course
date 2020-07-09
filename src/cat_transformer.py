c39448b0d6e89f466c9077dd260a66c97320fd2a
Victor Koppejan <victor.koppejan@dsm.com>
2020-07-09 15:28:53 +0200

    cat_transformer no has doc string

--- src/cat_transformer.py
+++ src/cat_transformer.py
@@ -3,6 +3,18 @@ from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OneHotEncoder
 
 def cat_pipeline():
+        """Create a categorical pipeline.
+
+        Imputes with most_frequent strategy followed by onehotencoding
+
+        Parameters
+        ----------
+        none
+
+        Returns
+        -------
+        cat_pipeline : return a categorical pipeline.
+        """
     cat_pipeline = Pipeline(steps=[
         ("impute", SimpleImputer(strategy="most_frequent")),
         ("encode", OneHotEncoder())
